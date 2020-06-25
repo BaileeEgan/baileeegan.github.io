dd63d1776b8cdef3c7d04edb2745694d9a3ea970
BaileeEgan <began3@nd.edu>
2020-06-25 15:28:03 -0500

    Generate Pelican site

--- theme/js/theme.js
+++ theme/js/theme.js
@@ -1,35 +1,12 @@
 $(document).ready(function(){
-	/*$(".highlight pre").each(function(id) {
-		var bolded = ["for ", " in "]
-		var text = $(".highlight pre:eq(" + id + ")").html()
-		var new_text = ""
+	$("img").click(function () {
+		var src = $(this).attr('src');
+		if (src.includes("#preview")) {
+			$("body").css("background", "#001A33");
+			var full = $('<img id="full_image">'); //Equivalent: $(document.createElement('img'))
+			full.attr('src', src.replace("#preview", ""));
+			full.appendTo('body');
+		}
 		
-			
-		var lines = text.split("\n")
-		$.each(lines, function (j, line) {
-			$.each(bolded, function(i, str) {	
-				line = line.replace(new RegExp(str, "g"), "<b>" + str + "</b>")
-			})
-			
-			if (line.indexOf("`") != -1) {
-				var bactic = line.match(/`((?:\\.|[^"\\])*)`/g)
-				line = line.replace(bactic, "<i class='code_highlight1'>" + bactic + "</i>")
-			}
-			
-			if (line.indexOf("#") > -1) {
-				var comment_index = line.indexOf("#")
-				var commented = line.substring(comment_index, line.length)
-				line = line.replace(commented, "<i class = 'code_comment'>" + commented + "</i>")
-			}
-			
-			if (j != 0) {
-				new_text += "\n"
-			}
-			new_text += line
-		})
-			
-		
-		
-		$(".highlight pre:eq(" + id + ")").html(new_text)
-	})*/
+	});
 })
