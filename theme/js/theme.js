5283467f713225d55f1435ec8874f16e92723ff4
BaileeEgan <began3@nd.edu>
2020-06-25 15:46:47 -0500

    Generate Pelican site

--- theme/js/theme.js
+++ theme/js/theme.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	$("img").click(function () {
 		var src = $(this).attr('src');
-		if (src.includes("#preview")) {
-			$("body").css("background", "#001A33");
-			var full = $('<img id="full_image">'); //Equivalent: $(document.createElement('img'))
-			full.attr('src', src.replace("#preview", ""));
-			full.appendTo('body');
+		var id = $(this).attr('id');
+		if (src.includes("#preview") && $("#modal-box").css("display") == "none") {
+			$("#modal-box").css("display", "block")
+			$("#modal-box").find("img").attr("src", src.replace("#preview", ""))
 		}
-		
 	});
+
+	$(".modal-close").click(function () {
+		$("#modal-box").css("display", "none")
+	})
 })
